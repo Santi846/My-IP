a91094ce16e61c4342561b2ecc1af850ac673258
Santiago Arocha <arochas846@gmail.com>
2024-04-07 22:09:09 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipEntry = Dns.GetHostEntry(hostName);
+            IPAddress[] addresses = ipEntry.AddressList;
+
+            Console.WriteLine("Your IP addresses:");
+            foreach (IPAddress address in addresses)
+            {
+                Console.WriteLine(address.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred: " + ex.Message);
+        }
+    }
+}
